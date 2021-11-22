5ce5c83945c8e60ebb96c53d3d39cde3ba99b530
seet <seet@github.com>
2021-11-21 18:41:53 -0800

    Added code for displaying random images on home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className='App'>
+   <Router>
+     <div>
+   <div className='App'>
       <h1>I'm Bored</h1>
     </div>
+    <Switch>
+      <Route path= '/'>
+        <Home/>
+      </Route>
+    </Switch>
+    </div>
+    </Router>
   );
 }
 
--- src/home.js
+++ src/home.js
@@ -1,1 +1,47 @@
+import React , {useState,useEffect,useCallback} from 'react';
+export default function Home() {
 
+    let url = 'https://api.unsplash.com/photos/random?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=gardening,swimming,card game,dance,write a poem,jigsaw puzzle,roller skate,cooking,photos with friends,watch a movie,picnic&count=10';
+    let [state,setState] =useState([])
+    
+    let imagesArray = new Array();
+    useEffect(() =>{
+      fetch(url)
+     .then((response) =>  (response.json()))
+     .then((data) => {
+     
+       
+        data.forEach(item =>{
+           imagesArray.push(item.urls.thumb);
+                     
+            }  
+                
+          )
+      
+       setState(imagesArray)
+       console.log({state})
+      
+        })
+    
+      .catch((error)=>{
+      console.error(error);
+  })
+},[url])
+      
+   
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      
+  
+     <div class='images'>
+     {state.map((state) => (
+        <img src={state} width='250' height='250' />
+    ))}
+     </div>
+    </main>
+  );
+        
+  };
+
+    
+  
