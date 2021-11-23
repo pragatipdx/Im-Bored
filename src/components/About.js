35532b4d88a8e1b1cda52f586e25ea7942fc6741
reeya52 <reeya@pdx.edu>
2021-11-22 17:39:50 -0800

    Extra features to about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,46 +9,68 @@ function About() {
       <div className="About">
           <br/>
           <h2 className="text-center abouthead">
-                <span className="border rounded p-2">
-                    <b>About us</b>
-                </span>
+            <span className="border rounded p-2">
+              <b>About us</b>
+            </span>
           </h2>
           <br/>
+          <div className="row p">
+            <div className="col-md-4 col-lg-4 col-sm-12">
+              <div className="d-flex justify-content-center aboutmain">
+                <div className="card-body">
+                  <div className="aboutProj">
+                    <p>
+                      I am Bored application is dashboard application to search for fun activities when you are bored.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="row">
             <div className="col-md-4 col-lg-4 col-sm-12">
-                <div className="max-auto card " style={{height: "28em"}}>
-                    <div className="card-img">
-                        <img src={Reeya_image} id="image1" className="img-fluid" alt="Profilepicture" style={{height: "15em"}} />
-                    </div>
-                    <div className="card-body">
-                      <h3>Reeya Kumbhojkar</h3>
-                        <p>Computer Science Graduate student from PSU</p>
-                    </div>
+              <div className="max-auto card " style={{height: "28em"}}>
+                <div className="card-img">
+                  <img src={Reeya_image} id="image1" className="img-fluid" alt="Profilepicture" style={{height: "15em"}} />
                 </div>
-            </div>
-            <div className="col-md-4 col-lg-4 col-sm-12">
-              <div className="max-auto card" style={{height: "28em"}}>
-                  <div className="card-img">
-                      <img src={image2} id="image2" className="img-fluid" alt="Profilepicture" style={{height: "15em"}} />
-                  </div>
-                  <div className="card-body">
-                      <h4>Pragati Rathore</h4>
-                         <p>Computer Science Graduate student from PSU</p>
-                  </div>
+                <div className="card-body">
+                  <h3>Reeya Kumbhojkar</h3>
+                    <p>Computer Science Graduate student from PSU</p>
+                    </div>
+                <div className="card-footer">
+                  <a href="https://www.linkedin.com/in/reeyakumbhojkar5/" className="card-link">My Linkedin Profile</a>
+                </div>
               </div>
             </div>
             <div className="col-md-4 col-lg-4 col-sm-12">
               <div className="max-auto card" style={{height: "28em"}}>
-                  <div className="card-img">
-                      <img src={image3} id="image3" className="img-fluid" alt="Profilepicture" style={{height: "15em"}} />
-                  </div>
-                  <div className="card-body">
-                      <h5>Priyanka Sithamraju</h5>
-                          <p>Computer Science Graduate student from PSU</p>
-                      </div>
-                  </div>
+                <div className="card-img">
+                  <img src={image2} id="image2" className="img-fluid" alt="Profilepicture" style={{height: "15em"}} />
+                </div>
+                <div className="card-body">
+                  <h4>Pragati Rathore</h4>
+                    <p>Computer Science Graduate student from PSU</p>
+                </div>
+                <div className="card-footer">
+                  <a href="https://www.linkedin.com/in/pragatirathore/" className="card-link">My Linkedin Profile</a>
+                </div>
               </div>
             </div>
+            <div className="col-md-4 col-lg-4 col-sm-12">
+              <div className="max-auto card" style={{height: "28em"}}>
+                <div className="card-img">
+                  <img src={image3} id="image3" className="img-fluid" alt="Profilepicture" style={{height: "15em"}} />
+                </div>
+                <div className="card-body">
+                  <h5>Priyanka Sithamraju</h5>
+                    <p>Computer Science Graduate student from PSU</p>
+                </div>
+                <div className="card-footer">
+                  <a href="https://www.linkedin.com/in/reeyakumbhojkar5/" className="card-link">My Linkedin Profile</a>
+                </div>
+              </div>
+            </div>
+          </div>
       </div>
     );
   }
