318c617cd540b9b6bbb983cf1d1069666be7294a
seet <seet@github.com>
2021-11-23 22:11:25 -0800

    final oputput of dynamically rendering images

--- src/components/GenerateImages.js
+++ src/components/GenerateImages.js
@@ -1,56 +1,43 @@
-  
-import React , {ReactFragment} from 'react';
-import {useState,useEffect,useCallback} from 'react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import { ImageListItemBar } from '@material-ui/core'
+import React, { ReactFragment } from "react";
+import { useState, useEffect, useCallback } from "react";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+import { ImageListItemBar } from "@material-ui/core";
 
-import GridUtil from './GridUtil';
- 
-  
-  const GenerateImages = (props)=> {
- 
+import GridUtil from "./GridUtil";
 
-    let url = props
-    console.log('wswx',url)
+const GenerateImages = (props) => {
+  let { url, title } = props;
 
-    let [state,setState] = useState([])
-    
-  
-       
-    fetch(url.data)
-    .then((response) =>  (response.json()))
+  let [state, setState] = useState([]);
+
+  fetch(url)
+    .then((response) => response.json())
     .then((data) => {
-        console.log(data,'pri')
-         setState(data.results[0].urls.thumb) 
+      setState(data.results[0].urls.thumb);
+    })
 
+    .catch((error) => {
+      console.error(error);
+    });
 
-       })
-   
-     .catch((error)=>{
-     console.error(error);
- })
-   
-    return( 
-        <>
-        <img
-        src={state}/>
-         <GridListTileBar
-           title='Recreational'
-           subtitle={<span>Activity Type</span>}
-           actionIcon={
-             <IconButton aria-label={`Best Place to learn`}>
-               <InfoIcon />
-             </IconButton>
-           }
-         />
-       </>
-    
-  )
-
-}
-export default GenerateImages
+  return (
+    <>
+      <img src={state} />
+      <GridListTileBar
+        title={title}
+        subtitle={<span>Activity Type</span>}
+        actionIcon={
+          <IconButton aria-label={`Best Place to learn`}>
+            <InfoIcon />
+          </IconButton>
+        }
+      />
+    </>
+  );
+};
+export default GenerateImages;
--- src/components/GridUtil.js
+++ src/components/GridUtil.js
@@ -1,76 +1,83 @@
-import React , {ReactFragment} from 'react';
-import {useState,useEffect,useCallback} from 'react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-import { ImageListItemBar } from '@material-ui/core'
-import GenerateImages from "./GenerateImages"
- 
+import React, { ReactFragment } from "react";
+import { useState, useEffect, useCallback } from "react";
+import GridList from "@material-ui/core/GridList";
+import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import IconButton from "@material-ui/core/IconButton";
+import InfoIcon from "@material-ui/icons/Info";
+import { ImageListItemBar } from "@material-ui/core";
+import GenerateImages from "./GenerateImages";
 
 let urls = new Array();
 let image_urls = new Array();
 const GridUtil = () => {
+  let education_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=education";
+  let recreational_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=swimming";
+  let social_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=friends";
+  let diy_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=jigsaw";
+  let charity_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=charity";
+  let cooking_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=cooking";
+  let busywork_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=busy work";
+  let random_url =
+    "https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=random";
 
- 
-
-
-  let education_url = 'https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=education'
-  let recreational_url = 'https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=swimming'
-  let social_url='https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=friends'
-  let diy_url = 'https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=jigsaw'
-  let charity_url = 'https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=charity'
-  let cooking_url='https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=cooking'
-  let busywork_url = 'https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=busy work'
-  let random_url = 'https://api.unsplash.com/search/photos?client_id=XVkqfP4oGQXxceYU3rJi2GIwtaAHPhCwosw1PdjOv7I&query=random'
-
-  
-  urls.push(education_url,recreational_url,social_url,diy_url,charity_url,cooking_url,busywork_url,random_url)
-  
-  
-  return (
-    
-    <div style={{margin: 'auto' }}>
-    <GridList cellHeight={180} >
-      <GridListTile key="Subheader" cols={2} rows={4} style={{ height: 'auto' }}>
-        <ListSubheader component="div">I'm Bored</ListSubheader>
-      </GridListTile>
-       <GridListTile>
-      <GenerateImages data={education_url}/>
-      </GridListTile>
-      <GridListTile>
-        
-       <GenerateImages data={recreational_url}/>
-       
-      </GridListTile>
-      <GridListTile>
-        <GenerateImages data={social_url}/>
-      </GridListTile>
-      <GridListTile>
-   
-        <GenerateImages data={diy_url}/>
-      </GridListTile>
-      <GridListTile>
-       <GenerateImages data={charity_url}/>
-      </GridListTile>
-      <GridListTile>
-      <GenerateImages data={cooking_url}/>
-      </GridListTile>
-      <GridListTile>
-       <GenerateImages data={busywork_url}/>
-      </GridListTile>
-      <GridListTile>
-       <GenerateImages data={random_url}/>
-      </GridListTile>
-     
-      
-
-    </GridList>
-  </div>
+  urls.push(
+    education_url,
+    recreational_url,
+    social_url,
+    diy_url,
+    charity_url,
+    cooking_url,
+    busywork_url,
+    random_url
   );
-}
- 
- 
+
+  return (
+    <div style={{ margin: "auto" }}>
+      <GridList cellHeight={180}>
+        <GridListTile
+          key="Subheader"
+          cols={2}
+          rows={4}
+          style={{ height: "auto" }}
+        >
+          <ListSubheader component="div">I'm Bored</ListSubheader>
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={education_url} title={"Education"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={recreational_url} title={"Recreation"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={social_url} title={"Social"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={diy_url} title={"DIY"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={charity_url} title={"Charity"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={cooking_url} title={"Cooking"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={busywork_url} title={"Busy Work"} />
+        </GridListTile>
+        <GridListTile>
+          <GenerateImages url={random_url} title={"Random"} />
+        </GridListTile>
+      </GridList>
+    </div>
+  );
+};
+
 export default GridUtil;
