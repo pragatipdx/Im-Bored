387163118707fb036dea50d6ac84ad9e6073425c
Sai priyanka seethamraju <seet@pdx.edu>
2021-11-25 13:01:12 -0800

    designed front page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ import React from "react";
 import GridUtil from "./GridUtil";
 
 function Home() {
-    return (
-      <div className="Home">
-        <FrontPage />
-      </div>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div className="Home">
+      <FrontPage />
+    </div>
+  );
+}
+
+export default Home;
